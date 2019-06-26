350f75126e33dcaa828c99a9d35d789313987605
txx <tangxiaoxuan2005@gmail.com>
2019-06-27 02:12:38 +0800

    feat(main): add gin and apis

--- main.go
+++ main.go
@@ -1,49 +1,70 @@
 package main
 
 import (
-	"context"
-	"log"
-	// "os"
-	"time"
-
-	pb "./proto"
-	"google.golang.org/grpc"
-)
-
-const (
-	address = "ac.testnet.libra.org:8000"
+	"github.com/gin-gonic/gin"
+	"strconv"
 )
 
 func main() {
-	conn, err := grpc.Dial(address, grpc.WithInsecure())
-	if err != nil {
-		log.Fatalf("did not connect: %v", err)
-	}
-	defer conn.Close()
+	db := NewDataBaseAdapter("root:test@(127.0.0.1:32773)/libra")
+	db.migration()
 
-	c := pb.NewAdmissionControlClient(conn)
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second * 100)
-	defer cancel()
+	r := gin.Default()
 
-	r, err := c.UpdateToLatestLedger(
-		ctx,
-		&pb.UpdateToLatestLedgerRequest{
-			ClientKnownVersion: 0,
-			RequestedItems: []*pb.RequestItem{
-				&pb.RequestItem {
-					RequestedItems: &pb.RequestItem_GetTransactionsRequest{
-						&pb.GetTransactionsRequest{
-							StartVersion: 0,
-							Limit:        100,
-							FetchEvents:  true,
-						},
-					},
-				},
-			},
-		})
+	r.GET("/version", func(c *gin.Context) {
+		offsetStr := c.DefaultQuery("offset", "0")
+		limitStr := c.DefaultQuery("limit", "20")
 
-	if err != nil {
-		log.Fatalf("could not greet: %v", err)
-	}
-	log.Printf("Greeting: %s", r.String())
+		offset, err1 := strconv.Atoi(offsetStr)
+		limit, err2 := strconv.Atoi(limitStr)
+
+		if err1 != nil || err2 != nil {
+			c.JSON(400, gin.H{"message": "bad request"})
+			return
+		}
+
+		c.JSON(200, db.GetVersions(offset, limit))
+	})
+
+	r.GET("/version/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		id64, err := strconv.ParseInt(id, 10, 64)
+
+		if err != nil {
+			c.JSON(400, gin.H{"message": "bad request"})
+			return
+		}
+
+		version := db.GetVersion(uint64(id64))
+		if version.ID == 0 {
+			c.JSON(404, gin.H{"message": "not found"})
+		} else {
+			c.JSON(200, version)
+		}
+	})
+
+	r.GET("/account/:address", func(c *gin.Context) {
+		address := c.Param("address")
+		_, err := hexToBytes(address)
+
+		if len(address) != 64 || err != nil {
+			c.JSON(400, gin.H{"message": "bad request"})
+			return
+		}
+
+		rpc := NewLibraRPC(nil)
+		r, err := rpc.GetAccountState(address)
+
+		if err != nil {
+			c.JSON(400, gin.H{"message": "bad request"})
+		} else {
+			if r == nil {
+				c.JSON(404, gin.H{"message": "not found"})
+			} else {
+				c.JSON(200, r)
+			}
+		}
+	})
+
+	r.Run()
 }
